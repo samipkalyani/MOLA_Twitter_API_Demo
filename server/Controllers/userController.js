f0b9f9aa21d5d340deb975083b975850c4a9e083
samip kalyani <samipkalyani@gmail.com>
2021-11-13 17:35:22 -0800

    fix

---
+++ server/Controllers/userController.js
@@ -0,0 +1,63 @@
+const https = require("https");
+const dotenv  = require('dotenv').config({path:`./config.env`});
+
+exports.getCheckUsername = (req, res) => {
+    var username=req.query.username
+    const username_options = {
+        hostname: 'api.twitter.com',
+        port: 443,
+        path: `/2/users/by/username/${username}?user.fields=id`,
+        method: 'GET',
+        headers: {
+            "User-Agent": "v2UserLookupJS",
+            "authorization": `Bearer ${process.env.BEARER_TOKEN}`
+        }
+    }
+    const twitterReq = https.request(username_options, twitterRes => {
+        twitterRes.on('data', d => {
+            let ans = JSON.parse(d.toString());
+            if(ans.errors){
+                ans={"exists":false}
+                return res.json(ans)
+            }
+            ans["exists"]=true
+            return res.json(ans)
+        })
+    })
+    twitterReq.on('error', error => {
+        let ans={"exists":false}
+        return res.json(ans)
+    })
+    twitterReq.end()
+};
+
+exports.getTweets = (req, res, next) => {
+    var user_id=req.query.user_id
+    const username_options = {
+        hostname: 'api.twitter.com',
+        port: 443,
+        path: `/2/users/${user_id}/tweets`,
+        method: 'GET',
+        headers: {
+            "User-Agent": "v2UserLookupJS",
+            "authorization": `Bearer ${process.env.BEARER_TOKEN}`
+        }
+    }
+    const twitterReq = https.request(username_options, twitterRes => {
+        twitterRes.on('data', d => {
+            let ans = JSON.parse(d.toString());
+            console.log(ans)
+            if(ans.meta.result_count>0){
+                return res.json(ans.data)
+            }
+            else{
+                return res.json([])
+            }
+        })
+    })
+    twitterReq.on('error', error => {
+        let ans={"exists":false}
+        return res.json(ans)
+    })
+    twitterReq.end()
+};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
-const https = require("https");
+//Global Modules
+const https = require("https")
+const path=require("path")
+
+//Custom Modules 
 const dotenv  = require('dotenv').config({path:`./config.env`});
 const express=require("express")
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const path=require("path")
-const fs=require("fs")
 
+//Using router
+const userRoutes=require("./Routes/userRoutes")
 
-const app=express();
+const app=express(); //app object
+//Using CORS for cross origin requests.
 const corsOptions = {
 	origin: "*"
 };
@@ -16,67 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, ".", "/")));
 
-app.get("/check_username",(req,res)=>{
-    var username=req.query.username
-    // var username="elonmusk"
-    const username_options = {
-        hostname: 'api.twitter.com',
-        port: 443,
-        path: `/2/users/by/username/${username}?user.fields=id`,
-        method: 'GET',
-        headers: {
-            "User-Agent": "v2UserLookupJS",
-            "authorization": `Bearer ${process.env.BEARER_TOKEN}`
-        }
-    }
-    const twitterReq = https.request(username_options, twitterRes => {
-        twitterRes.on('data', d => {
-            let ans = JSON.parse(d.toString());
-            if(ans.errors){
-                ans={"exists":false}
-                return res.json(ans)
-            }
-            ans["exists"]=true
-            return res.json(ans)
-        })
-    })
-    twitterReq.on('error', error => {
-        let ans={"exists":false}
-        return res.json(ans)
-    })
-    twitterReq.end()
-})
+app.use(userRoutes)
 
-app.get("/tweets",(req,res)=>{
-    var user_id=req.query.user_id
-    // var user_id="44196397"
-    const username_options = {
-        hostname: 'api.twitter.com',
-        port: 443,
-        path: `/2/users/${user_id}/tweets`,
-        method: 'GET',
-        headers: {
-            "User-Agent": "v2UserLookupJS",
-            "authorization": `Bearer ${process.env.BEARER_TOKEN}`
-        }
-    }
-    const twitterReq = https.request(username_options, twitterRes => {
-        twitterRes.on('data', d => {
-            let ans = JSON.parse(d.toString());
-            console.log(ans)
-            if(ans.meta.result_count>0){
-                return res.json(ans.data)
-            }
-            else{
-                return res.json([])
-            }
-        })
-    })
-    twitterReq.on('error', error => {
-        let ans={"exists":false}
-        return res.json(ans)
-    })
-    twitterReq.end()
-})
-
+//Listen on 8000
 app.listen(8000)
